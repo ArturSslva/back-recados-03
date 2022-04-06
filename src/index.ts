7b75c36d27eb2430dec7792dbe4cc85cdb239c5e
ArturS <arturs.dasilva21@gmail.com>
2022-04-05 23:07:51 -0300

    deploy

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(cors());
 
-app.get('/errands', (request: Request, response: Response) => {
+app.get('/', (request: Request, response: Response) => {
     return response.send('API UP');
 });
 
